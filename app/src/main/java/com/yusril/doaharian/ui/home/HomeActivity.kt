149fc2ca4b4024c46a8fc593f417719a43c231e1
yysrll <yusril.uzumaki@gmail.com>
2021-05-18 16:31:26 +0800

    prepare project

---
+++ app/src/main/java/com/yusril/doaharian/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.yusril.doaharian.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yusril.doaharian.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
